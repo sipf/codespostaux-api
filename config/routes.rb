07a0f3cda461aa4f2326b7e4c3b6d5766e28832e
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2016-07-18 11:51:58 -1000

    Ajout de la recherche de code.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :communes
-  resources :iles
-  resources :archipels
+  # resources :communes
+  # resources :iles
+  # resources :archipels
+
+  namespace :v1 do
+    resources :codes, only: [:index]      
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
